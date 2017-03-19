ef491477aebf79ae42b3b043126cc0ba4b07bde5
indianstartups <indianstartups001@gmail.com>
2017-03-18 21:48:17 -0700

    Update developer.js

--- developer.js
+++ developer.js
@@ -1,1 +1,10 @@
-console.log('git hub developer alert');
+console.log('started');
+$(document).ready(function() {   
+ var html = ' <div class="item">';
+ html += '<div class="col-md-12 text-center" style="vertical-align:middle">';
+ html += '<a href="http://www.w3schools.com"  target="_blank">';
+ html += '<img src="http://lib.virtualdatastore.com/indianstartups.com/img/events/numberz_banner.png"  /></a>';
+ html += '</div></div>';
+ $("#innerCarousel").append(html);
+});
+console.log('finished');
