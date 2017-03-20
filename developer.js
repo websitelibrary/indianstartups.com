53f57cba37c1171a0590cbc9fa2d4f009d332a9b
indianstartups <indianstartups001@gmail.com>
2017-03-20 13:59:39 -0700

    Update developer.js

--- developer.js
+++ developer.js
@@ -2,7 +2,7 @@ console.log('started');
 $(document).ready(function() {   
  var html = ' <div class="item">';
  html += '<div class="col-md-12 text-center" style="vertical-align:middle">';
- html += '<a href="http://www.w3schools.com"  target="_blank">';
+ html += '<a href="https://www.techinasia.com/15-topfunded-startups-india"  target="_blank">';
  html += '<img src="https://cdn.techinasia.com/wp-content/uploads/2015/11/Top-Funded-India-eCommerce-Feature-Image.png"  /></a>';
  html += '</div></div>';
  $("#innerCarousel").append(html);
